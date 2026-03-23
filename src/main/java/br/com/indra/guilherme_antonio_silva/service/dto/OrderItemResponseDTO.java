be76d6b84d5b0d59a730d752c845041a461fdc66
Guilherme Antonio Silva <guiantoni596@gmail.com>
2026-03-23 00:20:04 -0300

    feat(orders): criar DTO de resposta para itens do pedido

---
+++ src/main/java/br/com/indra/guilherme_antonio_silva/service/dto/OrderItemResponseDTO.java
@@ -0,0 +1,23 @@
+package br.com.indra.guilherme_antonio_silva.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemResponseDTO {
+
+    private Long id;
+    private Long produtoId;
+    private String produtoNome;
+    private Integer quantidade;
+    private BigDecimal precoUnitarioSnapshot;
+    private BigDecimal totalLinha;
+}
+
